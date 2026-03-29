857486a64aff5ccbc22e377e9e86c252036c3917
Natan Addis <nasamri33@gmail.com>
2026-03-29 11:12:29 +0300

    fix: schema change on attendance and some changes on the config file

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Main color: Blue (oklch(0.55 0.18 250)) */
-  /* config options here */
+  // /* Main color: Blue (oklch(0.55 0.18 250)) */
+  // /* config options here 
 };
 
 export default nextConfig;
